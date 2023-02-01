49b2eae4071ecb4f8cca2e347d9b38325931ce45
Alielhaba <118537759+Alielhaba@users.noreply.github.com>
2023-02-01 17:27:42 +0200

    Update Jenkins.tf

--- Jenkins.tf
+++ Jenkins.tf
@@ -1,1 +1,21 @@
-ls
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
+}
